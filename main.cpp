34a8f0efb7fce67397eb4486e4a7cbefb565a799
Michał Starski <michalstarski99@gmail.com>
2019-11-14 22:59:19 +0100

    update

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
 	window.setFramerateLimit(60);	//Limit klatek do 60
 
 	sf::RectangleShape* rec = new sf::RectangleShape(sf::Vector2f(0.0f, 0.0f));
-	GameObject<Component> go = GameObject<Component>(rec, sf::Vector2f(0.0f, 0.0f));
+	GameObject go = GameObject(rec, sf::Vector2f(0.0f, 0.0f));
 	
 	//Uzupelnienie animacji klatkami i ich czasami
 
